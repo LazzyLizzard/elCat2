8fcd5ab572e6f9f29fcd953773317a7b7671cfea
sf <lazy.lizard@yandex.ru>
2017-04-24 16:54:54 +0300

    dir rename

---
+++ src/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+//import { bindActionCreators } from 'redux'
+//import { connect } from 'react-redux'
+
+
+class App extends Component {
+
+    render() {
+//        const ml = this.props.ml.manufacturers;
+//        console.log(this.props);
+//        const { ml, isLoading, trTypesData } = this.props;
+        return <div>
+            Elcat
+        </div>
+    }
+
+
+}
+
+export default App;
