32c5115f454d265792f0617dde18498dca15d5fb
sf <lazy.lizard@yandex.ru>
2017-04-24 15:46:13 +0300

    emvironment

---
+++ scr/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+
+//import storeStructure from './store/sStruct'
+import configureStore from './store/configureStore'
+//
+
+//const initialState = () => {
+//    return {
+//        isLoading: false
+//    }
+//}
+const store = configureStore();
+
+render(
+    <Provider store={store}>
+        <div className='app'>
+            <App />
+        </div>
+    </Provider>,
+    document.getElementById('root')
+)
+
